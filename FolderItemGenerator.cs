d1e9aedca84c3dcfd475bd2f7646cee1c1d486e2
Petar <psihosocial@abv.bg>
2019-05-27 20:05:56 +0300

    Added new items

---
+++ FolderItemGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+
+namespace Helpers
+{
+    /// <summary>
+    /// Gets a path, traverses it and returs a string, containing references to all files.
+    /// </summary>
+    class FolderItemGenerator
+    {
+
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+
+        private void WorkWith(string file)
+        {
+            string containingFolder = Directory.GetParent(file).Name;
+            string upperFolder = @"images/italy";
+
+
+            string cssClass = 
+                containingFolder == "01.04 Welcome Fortes" ? "Vicenza" :
+                containingFolder == "03.04 saf" ? "Padova" :
+                containingFolder == "04.04ITIS Enrico Fermi" ? "Vicenza" :
+                containingFolder == "05.04 Frantoio di Valnogaredo  +I_ITT G. Marconi" ? "Padova" :
+                containingFolder == "06.04 Venice" ? "Venice" :
+                containingFolder == "07.04 Pisa" || containingFolder == "07.04 Florence" ? "Florence" :
+                containingFolder == "08.04 Padova" ? "Padova" :
+                containingFolder == "11.04 IIS Silvio Ceccato + Castles of Romeo and Juliet" ? "Vicenza" :
+                containingFolder == "12.04 Verona" ? "Verona" :
+                containingFolder == "16.04 Telwin = La Costa" ? "Vicenza" : "ERROR";
+
+                                 
+            string str = $"<li class=\"mix {cssClass}\">\n" +
+                         $"      <a class=\"portfolio-popup\" href=\"{upperFolder + @"/" + containingFolder + @"/" + Path.GetFileName(file)}\">\n" +
+                         $"          <img src = \"{upperFolder + @"/" + containingFolder + @"/" + Path.GetFileName(file)}\" alt=\"\" />\n" +
+                         "          <div class=\"item-overly\">\n" +
+                         "              <div class=\"position-center\">\n" +
+                         "                  <h4></h4>\n" +
+                         "                  <p></p>\n" +
+                         "              </div>\n" +
+                         "          </div>\n" +
+                         "      </a>\n" +
+                         "</li>\n";
+            builder.Append(str + "\n");
+        }
+
+
+        public FolderItemGenerator()
+        {
+            //@"C:\Users\Shannarra\Desktop\vicenza\images\italy\"
+            string[] files = Directory.GetFiles(@"C:\Users\Shannarra\Desktop\vicenza\images\italy\", "*.*", SearchOption.AllDirectories).Select(x => Path.GetDirectoryName(x) + "/" + Path.GetFileName(x)).ToArray();
+
+            foreach (var item in files)
+            {
+                WorkWith(item);
+            }
+
+            using (StreamWriter w = new StreamWriter(@".\..\..\imgs.xml"))
+                w.WriteLine(builder.ToString());
+
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,90 @@
+﻿namespace WorkerApplication
+{
+    using System;
+
+    public static class Utility
+    {
+        /// <summary>
+        /// Redirects to another XAML view.
+        /// </summary>
+        /// <param name="viewType">The type of the view. To get it use the <see cref="typeof()"/> function.</param>
+        public static void RedirectTo(System.Type viewType)
+        {
+            Windows.UI.Xaml.Controls.Frame frame = new Windows.UI.Xaml.Controls.Frame();
+            frame.Navigate(viewType, null);
+            Windows.UI.Xaml.Window.Current.Content = frame;
+        }
+
+        /// <summary>
+        /// Displays a <see cref="Windows.UI.Xaml.Controls.ContentDialog"/>.
+        /// </summary>
+        /// <param name="title">The dialog's title.</param>
+        /// <param name="textContent">The dialog's text contents.</param>
+        /// <param name="primaryButton">The primary button text.</param>
+        /// <param name="secondaryButton">The secondary button text.</param>
+        /// <param name="defaultBtn">Which <see cref="Windows.UI.Xaml.Controls.ContentDialogButton"/> will be primary?</param>
+        /// <returns>bool? result of displaying.</returns>
+        public static bool? ShowDialog
+            (string title, string primaryButton, string secondaryButton, string textContent = "",
+            Windows.UI.Xaml.Controls.ContentDialogButton? defaultBtn = Windows.UI.Xaml.Controls.ContentDialogButton.Primary)
+        {
+            Windows.UI.Xaml.Controls.ContentDialog dialog = new Windows.UI.Xaml.Controls.ContentDialog
+            {
+                Title = title,
+                Content = textContent,
+                PrimaryButtonText = primaryButton,
+                SecondaryButtonText = secondaryButton,
+                CloseButtonText = "Cancel",
+                DefaultButton = (Windows.UI.Xaml.Controls.ContentDialogButton)defaultBtn
+            };
+
+            Windows.Foundation.IAsyncOperation<Windows.UI.Xaml.Controls.ContentDialogResult> operation = dialog.ShowAsync(); //run the dialog asynchronously
+            if (operation.Status == Windows.Foundation.AsyncStatus.Started) //check the status
+                return true;
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the given <paramref name="pic"/> with another picture.
+        /// </summary>
+        /// <param name="pic"><see cref="Windows.UI.Xaml.Controls.PersonPicture"/> to be changed</param>
+        /// <returns>bool? result of the job done.</returns>
+        public static async System.Threading.Tasks.Task<bool?> SetPictureAsync(Windows.UI.Xaml.Controls.PersonPicture pic)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary
+            };
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                var image = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+                image.SetSource(stream);
+                pic.ProfilePicture = image;
+                return true; //picture changed successfully
+            }
+            else
+                return null; //the file is null
+
+        }
+
+
+        /// <summary>
+        /// Returns a Brush for the given ARGB values.
+        /// </summary>
+        /// <param name="r">Red</param>
+        /// <param name="g">Blue</param>
+        /// <param name="b">Green</param>
+        /// <returns></returns>
+        public static Windows.UI.Xaml.Media.Brush ConvertColor(int r, int g, int b)
+        {
+            return new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(Convert.ToByte(255), Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b)));
+        }
+    }
+}
